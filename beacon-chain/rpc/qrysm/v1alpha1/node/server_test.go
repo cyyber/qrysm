44983ed7e3ff69877b73d0548e39b477f7fd2c8f
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2026-08-04 20:15:20 +0400

    test: align async and peer tests with upstream

--- beacon-chain/rpc/qrysm/v1alpha1/node/server_test.go
+++ beacon-chain/rpc/qrysm/v1alpha1/node/server_test.go
@@ -126,11 +126,9 @@ func TestNodeServer_GetPeer(t *testing.T) {
 	}
 	qrysmpb.RegisterNodeServer(server, ns)
 	reflection.Register(server)
-	firstPeer := "16Uiu2HAkyWZ4Ni1TpvDS8dPxsozmHY85KaiFjodQuV6Tz5tkHVeR"
-
-	res, err := ns.GetPeer(context.Background(), &qrysmpb.PeerRequest{PeerId: firstPeer})
+	res, err := ns.GetPeer(context.Background(), &qrysmpb.PeerRequest{PeerId: mockP2p.MockRawPeerId0})
 	require.NoError(t, err)
-	assert.Equal(t, firstPeer, res.PeerId, "Unexpected peer ID")
+	assert.Equal(t, "16Uiu2HAkyWZ4Ni1TpvDS8dPxsozmHY85KaiFjodQuV6Tz5tkHVeR" /* first peer's raw id */, res.PeerId, "Unexpected peer ID")
 	assert.Equal(t, int(qrysmpb.PeerDirection_INBOUND), int(res.Direction), "Expected 1st peer to be an inbound connection")
 	assert.Equal(t, qrysmpb.ConnectionState_CONNECTED, res.ConnectionState, "Expected peer to be connected")
 }
@@ -147,12 +145,25 @@ func TestNodeServer_ListPeers(t *testing.T) {
 	res, err := ns.ListPeers(context.Background(), &emptypb.Empty{})
 	require.NoError(t, err)
 	assert.Equal(t, 2, len(res.Peers))
-	directions := map[qrysmpb.PeerDirection]int{}
-	for _, peer := range res.Peers {
-		directions[peer.Direction]++
+
+	var (
+		firstPeer  *qrysmpb.Peer
+		secondPeer *qrysmpb.Peer
+	)
+
+	for _, p := range res.Peers {
+		if p.PeerId == mockP2p.MockRawPeerId0 {
+			firstPeer = p
+		}
+		if p.PeerId == mockP2p.MockRawPeerId1 {
+			secondPeer = p
+		}
 	}
-	assert.Equal(t, 1, directions[qrysmpb.PeerDirection_INBOUND])
-	assert.Equal(t, 1, directions[qrysmpb.PeerDirection_OUTBOUND])
+
+	assert.NotNil(t, firstPeer)
+	assert.NotNil(t, secondPeer)
+	assert.Equal(t, int(qrysmpb.PeerDirection_INBOUND), int(firstPeer.Direction))
+	assert.Equal(t, int(qrysmpb.PeerDirection_OUTBOUND), int(secondPeer.Direction))
 }
 
 func TestNodeServer_GetExecutionConnectionStatus(t *testing.T) {
