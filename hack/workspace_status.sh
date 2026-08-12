7c9e39d1b0a2770bf533c2b628311a25d960cad8
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2026-08-12 12:18:32 +0400

    Honor SOURCE_DATE_EPOCH in build metadata

--- hack/workspace_status.sh
+++ hack/workspace_status.sh
@@ -3,7 +3,8 @@
 # Note: The STABLE_ prefix will force a relink when the value changes when using rules_go x_defs.
 
 echo STABLE_GIT_COMMIT "$(git rev-parse HEAD)"
-echo DATE "$(date --rfc-3339=seconds --utc)"
-echo DATE_UNIX "$(date --utc +%s)"
+BUILD_EPOCH="${SOURCE_DATE_EPOCH:-$(date --utc +%s)}"
+echo DATE "$(date --date="@${BUILD_EPOCH}" --rfc-3339=seconds --utc)"
+echo DATE_UNIX "${BUILD_EPOCH}"
 echo DOCKER_TAG "$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)"
 echo STABLE_GIT_TAG "$(git describe --tags --abbrev=0)"
